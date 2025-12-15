823400d8cbb433a1614059d39d121fb92115e79d
Guilherme Lima <ghduep@outlook.com>
2025-12-15 07:35:51 -0300

    permutating a real long word with an algorithm

--- perm01.py
+++ perm01.py
@@ -1,3 +1,18 @@
 from itertools import permutations
+from collections import defaultdict
+from math import factorial
 
-print(permutations("disc"))
+def how_many_permutations(text: str) -> int:
+    letters = defaultdict()
+    for letter in text:
+        letters[letter] += 1
+    numerator = factorial(len(text))
+    factors = [factorial(count) for count in letters.values()]
+    denominator = 1
+    for factor in factors:
+        denominator *= factor
+
+    return numerator // denominator
+    
+print(how_many_permutations("paralelepipedo"))
+
